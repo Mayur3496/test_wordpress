f73711bc56353efda3aeb4c860afe90c6aee0ee1
Mayur <mayur@acquaintsoft.com>
2016-10-20 17:56:50 +0530

    sfd

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'test_git_mayur');
 
 /** MySQL database username */
-define('DB_USER', 'root');1
+define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'root');
